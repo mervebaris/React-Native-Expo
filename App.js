85412b558af92c59e247b736c37572201bfa06b6
Merve Barış <mrvbaris1@gmail.com>
2024-02-22 15:52:29 +0300

    expıo

--- App.js
+++ App.js
@@ -4,11 +4,37 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={{ fontWeight: "bold", fontsize: "18", color: "pink" }}>
+      {/* <Text style={{ fontWeight: "bold", fontsize: "18", color: "pink" }}>
         <Text style={{ fontSize: "20", color: "purple" }}>Hellüğ </Text>
         Merve Barış
-      </Text>
+      </Text> */}
       <StatusBar style="auto" />
+
+      <View
+        style={{
+          width: 200,
+          height: 200,
+          backgroundColor: "#F588E2",
+          borderRadius: 20,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text> barış</Text>
+        <View
+          style={{
+            width: 100,
+            height: 100,
+            backgroundColor: "#B8B8F5",
+            borderRadius: 10,
+            margin: 5,
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Text> merve</Text>
+        </View>
+      </View>
     </View>
   );
 }
